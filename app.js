3f9b26e5786ea6dca249754fa767e17c9c93611c
Abdulaziz alsunaydi <azozb997@gmail.com>
2022-02-09 00:49:02 +0300

    added score for each player and the score saved on local storage and a button to reset that score

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 
 const result = document.getElementById('result');
 const resultBtn = document.getElementById("btn-submit")
+const resetBtn = document.getElementById('reset')
+let scoreP1 = document.getElementById('scoreP1')
+let scoreP2 = document.getElementById('scoreP2')
+let countP1 = localStorage.getItem(0);
+let countP2 = localStorage.getItem(1);
+
+countP1 != null? scoreP1.innerHTML = "Score:"+  countP1: ""
+countP2 != null? scoreP2.innerHTML = "Score:"+  countP2: ""
 
 
+/* EventListener  */
+resetBtn.addEventListener('click',() => {
+    localStorage.clear();
+    window.location.href = '/'
+    
+    
+    console.log("load");
+})
 resultBtn.addEventListener('click', (e) =>{
 e.preventDefault()
 input()
 })
+
+
+/* Functions  */
 
 const input = () => {
 
  
     let playerInputOne = document.getElementById('input-player-one').value;
     let playerInputTwo = document.getElementById('input-player-two').value;
+
     if(playerInputOne === "" && playerInputTwo === ""){
-        result.innerHTML = "Enter A Number"
+        result.innerHTML = "Enter A Number of Two Digits"
     }
     else{
 
@@ -23,20 +43,33 @@ const input = () => {
 }
 
 const check = (playerNumberOne,playerNumberTwo) => {  
-    const number = Math.floor(Math.random(1) * 100)
+    const number = Math.floor(Math.random(1) * 100) // Random generated number
     const array = [playerNumberOne,playerNumberTwo]
   const closestNumber = closestTo(number,array)
 
 
     if(playerNumberOne === closestNumber){
+        localStorage.getItem(0,countP1)
+           countP1++;
+           localStorage.setItem(0,parseInt(countP1))
+
+        scoreP1.innerHTML = "Score:" + countP1
         result.innerHTML = `Player one Wins
         the number is: ${number}`
+
     }
     if(playerNumberTwo === closestNumber){
+        countP2
+        countP2++;
+        localStorage.setItem(1,parseInt(countP2))
+
+       
+        scoreP2.innerHTML = "Score:" + countP2
+
         result.innerHTML = `Player Two Wins
         the number is: ${number}`
     }
-    if(number === playerNumberOne && number === playerNumberTwo){
+    if(playerNumberOne === closestNumber && playerNumberTwo === closestNumber){
         result.innerHTML = `Draw
         the number is: ${number}`
     }
